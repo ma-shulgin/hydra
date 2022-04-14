7cb695404d852be733c8a7a2bd3d0fbc422a0b8c
Mikhail Shulgin <mashulgin@edu.hse.ru>
2022-04-14 12:23:31 +0300

    last hope

--- scripts/docker-publish.sh
+++ scripts/docker-publish.sh
@@ -5,11 +5,11 @@ for p in "$@"; do
     target="${p#hydra-}"
     version=$(node -p "require('./lerna.json').version")
     major=$(echo $version | cut -d '.' -f1)
-
-    docker build . --target "$target" \
-        -t subsquid/${p}:${version} \
-        -t subsquid/${p}:${major} \
-        -t subsquid/${p}:${release:-next} || exit 1
-
-    docker push subsquid/${p} --all-tags || exit 1
+    
+    docker buildx build . --platform "linux/amd64,linux/arm64" \
+        --push \
+        --target "$target" \
+        -t mashulgin/${p}:${version} \
+        -t mashulgin/${p}:${major} \
+        -t mashulgin/${p}:${release:-next} || exit 1
 done
